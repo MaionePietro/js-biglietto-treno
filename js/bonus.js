d7e3c3e78721b237e52678aaf107aaf8eb4956e1
MaionePietro <pietromaionespam@gmail.com>
2023-01-31 17:31:42 +0100

    FIX 2 file for js

--- js/bonus.js
+++ js/bonus.js
@@ -1,13 +1,23 @@
-// 0: Chiedere nome e cognome 
-
+alert('Benvenuto in Trenitalia, i nostri treni ti porteranno a destinazione con 10 anni di ritardo');
 let nome, cognome;
 nome = prompt('inserisci il tuo nome');
+document.getElementById('nome_id').innerHTML = nome;
+
 cognome = prompt('inserisci ul tuo cognome');
+document.getElementById('cognome_id').innerHTML = cognome;
+
+let partenza, arrivo;
+partenza = prompt('stazione partenza');
+document.getElementById('partenza_id').innerHTML = partenza;
+
+arrivo = prompt('stazione arrivo');
+document.getElementById('arrivo_id').innerHTML = arrivo;
 
 // 1: Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere
 
 let km = prompt('Quanti chilometri devi percorrere ?');
 km = parseInt(km);
+document.getElementById('km_id').innerHTML = km + ' km';
 console.log(km);
 
 // 2: Chiedere l'età del passeggero
@@ -17,27 +27,28 @@ anni = parseInt(anni);
 console.log(anni);
 
 // 3: Calcolare il prezzo totale dei kilometri
-
 const kmPrezzo = (0.21 );
 console.log(kmPrezzo);
 
 const totKm = (km * kmPrezzo);
+document.getElementById('totKm_id').innerHTML = totKm + ' tot €';
 console.log('prezzo chilometri: ' + totKm + ' €');
 
 // 4: applicare sconto in base all'età 20% junior e 40% senior
-
 let sconto, prezzoFinale, prezzoDecimali;
+
 if(anni <= 18){
     sconto = (totKm / 100) * 20;
     prezzoFinale = totKm - sconto;
+    document.getElementById('sconto_id').innerHTML = ('20%');
     prezzoDecimali = prezzoFinale.toFixed(2);
     console.log('prezzo finale: ' + prezzoDecimali + '€');
+    document.getElementById('prezzo_id').innerHTML = (prezzoDecimali + ' €');
 }else if(anni >= 65){
     sconto = (totKm / 100) * 40;
     prezzoFinale = totKm - sconto;
+    document.getElementById('sconto_id').innerHTML = ('40%');
     prezzoDecimali = prezzoFinale.toFixed(2);
     console.log('prezzo finale: ' + prezzoDecimali + '€');
+    document.getElementById('prezzo_id').innerHTML = (prezzoDecimali + ' €');
 }
-
-
-
